b6254c35bc3369d6ec8065e00909b0ca0dd391fd
Filip Roessner <filip.roessner@gmail.com>
2025-12-23 13:00:46 +0100

    Delete personalwebsite directory

--- personalwebsite/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Danke für deine Nachricht! Ich melde mich bald bei dir.");
-});
