d64755053a05d99f642af16285e45b251f4d9b51
Filip Roessner <filip.roessner@gmail.com>
2025-12-25 20:04:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Danke für deine Nachricht! Ich melde mich bald bei dir.");
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+    });
 });
